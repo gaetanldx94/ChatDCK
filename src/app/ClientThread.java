161eec71c39c01796ce4e8a4f6ab22058bcb5089
Ledoux Gaétan <41428973+gaetanldx94@users.noreply.github.com>
2023-05-10 09:54:03 +0200

    Add files via upload

--- src/app/ClientThread.java
+++ src/app/ClientThread.java
@@ -2,10 +2,12 @@ package src.app;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.Writer;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.logging.Logger;
@@ -108,23 +110,20 @@ public class ClientThread extends Thread
 					tmpListRank.add(this.pseudo + " : " + inputLine.split(":")[1]);
 					server.sendMsg(this.pseudo + " a fais " + inputLine.split(":")[1] + " bravo à lui !!");
 
-					File rankSave         = new File(RANK_SAVE);
-					PrintWriter print     = new PrintWriter(rankSave);
+					File file = new File(RANK_SAVE);
+					Writer writer = new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
 					StringBuilder builder = new StringBuilder();
-
-					if(!rankSave.exists())
-						rankSave.createNewFile();
 
 					for(int i = 0; i < tmpListRank.size(); i++)
 					{
 						builder.append(tmpListRank.get(i) + "\n");
 					}
 
-					print.write(builder.toString());
+					writer.append(builder.toString());
 					tmpListRank.sort(new RankComparator());
 					server.setTimeRank(tmpListRank);
 
-					print.close();
+					writer.close();
 				}
 				else
 				{
